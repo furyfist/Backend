dcbd90d6e710e2bc5e10167f0e980433b9ab35de
Himanshu <himanshu290304@gmail.com>
2025-10-17 01:27:08 +0530

    Modules in NodeJS

---
+++ hello.js
@@ -0,0 +1,6 @@
+
+const {add, substract} = require("./math")
+console.log("yooooooo")
+
+console.log("the addition is", add(2,3))
+console.log("the substraction is", substract(2,3))
---
+++ math.js
@@ -0,0 +1,14 @@
+// function add (a,b) {
+//     return a + b;
+// }
+
+// function substract(a,b) {
+//     return a - b;
+// }
+
+exports.add = (a,b) => a+b;
+exports.substract = (a,b) => a-b;
+// module.exports = {
+//     add, 
+//     substract
+// };
