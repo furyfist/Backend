175f1470d89d909f0f878f93535e4d43bdedb751
Himanshu <himanshu290304@gmail.com>
2025-10-17 12:24:36 +0530

    Starting Express

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 
-const myserver = http.createServer((req,res) => {
-    const log = `${Date.now()} : New request received \n ${req.url}`
-    if(req.url = "/favicon.ico"){
-        res.writeHead(204)
-    }
-    switch(req.url){
-        case "/" : 
-            res.end("HomePage");
-            break;
-        case "/about" :
-            res.end("This is the furyfist")
-            break;
-    }
-    fs.appendFile("log.txt", log, (err,data) => {
-        res.end("hello from server again");
-    }) 
-});
+const app = express()
 
-myserver.listen(8000, () => console.log("dn"));
+app.get("/", (req,res) => {
+    console.log("hii this is home page");
+    return res.send ("hii this is home page");
+})
+
+app.get("/about", (req,res) => {
+    console.log("hii this is about page");
+    return res.send ("hii this is about page");
+})
+
+app.listen(8000)
